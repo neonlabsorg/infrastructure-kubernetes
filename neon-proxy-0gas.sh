fb8bccc246736872e7a2ab7761761c67e68fe826
Sergei Bondar <mr.bondsv@gmail.com>
2025-04-25 18:47:58 +0400

    fix

--- neon-proxy-0gas.sh
+++ neon-proxy-0gas.sh
@@ -259,7 +259,7 @@ kubectl create namespace $NAMESPACE 2>/dev/null
   --history-max 3 \
   --set controller.service.type=$INGRESS_SERVICE_TYPE \
   --set controller.allowSnippetAnnotations=true \
-  --controller.admissionWebhooks.enabled=false \
+  --set controller.admissionWebhooks.enabled=false \
   --set controller.service.nodePorts.http=32080 \
   --set controller.service.nodePorts.https=32443  1>/dev/null
 }
